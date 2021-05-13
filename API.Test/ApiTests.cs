728d92c7d6cad09cfd10e710b55a1b30c04ae781
Jonathan Freeman <freemanj6@gmail.com>
2021-05-12 19:42:51 -0500

    added nunit testing framework

---
+++ API.Test/ApiTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace DatingApp
+{
+    public class ApiTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
